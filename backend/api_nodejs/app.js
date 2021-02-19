9ebdfc7cb0fd240ad35a37e11b911d7bd97bf499
Antonio Miranda <netomantonio@gmail.com>
2021-02-18 21:00:32 -0300

    created node application backend, including express packages and body-parsers

---
+++ backend/api_nodejs/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const indexRoute = require('./Routes/index');
+
+//BODY PARSER - necessário para reconhecer e poder extrair 
+//as informações vindas do objeto json
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', indexRoute);
+
+app.listen(3000);
+
+module.exports = app;
